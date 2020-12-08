913a9b8c2dbb2f68df2f9b02dcefc407218a95e8
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-08 21:42:19 +0000

    added test infrastructure and tests for get_birth_year

---
+++ tests/testthat/test-get_birth_year.R
@@ -0,0 +1,17 @@
+test_that("get_birth_year works", {
+    a <- decompose_cnp(1940616346114)
+    expect_equal(get_birth_year(a), "1994")
+
+    b <- decompose_cnp(7041218318525)
+    expect_equal(get_birth_year(b), "2004 sau 1904")
+    expect_equal(get_birth_year(b, lang = "EN"), "2004 or 1904")
+
+    c <- decompose_cnp(6201206018078)
+    expect_equal(get_birth_year(c), "2020")
+
+    d <- decompose_cnp(7321206018072)
+    expect_equal(get_birth_year(d), "1932")
+
+    e <- decompose_cnp(49804232603242)
+    expect_equal(get_birth_year(e), "1898")
+})
