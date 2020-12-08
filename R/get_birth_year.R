7915e6c7d2b8a7a457f7ca5c9a75020b97e8459f
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-08 12:49:31 +0000

    update and doc get_birth_year()

--- R/get_birth_year.R
+++ R/get_birth_year.R
@@ -2,7 +2,10 @@
 #'
 #' This function infers the year of birth based on the code for gender - which
 #' is indicative of the century of birth - and the `AA` component denoting the
-#' last 2 digits of the birth year.
+#' last 2 digits of the birth year. The main purpose of this function is to help
+#' the `interpret_cnp()` functionality. Thus, the year is returned as string,
+#' especially since for residents not born in Romania, there might be two
+#' possible valid values for the birth year.
 #'
 #' @param cnp_dec a named character vector representing a decomposed Personal
 #'     Numeric Code
@@ -11,20 +14,24 @@
 #' @export
 #'
 #' @examples
+#' a <- decompose_cnp(1940616346114)
+#' get_birth_year(a)
+#' b <- decompose_cnp(7041218318525)
+#' get_birth_year(b)
 get_birth_year <- function(cnp_dec) {
 
     if (cnp_dec["S"] %in% as.character(c(1, 2))) {
-        birth_year <- stringr::str_c(19, cnp_dec["AA"]) %>% as.integer()
+        birth_year <- stringr::str_c(19, cnp_dec["AA"])
         return(birth_year)
     }
 
     if (cnp_dec["S"] %in% as.character(c(3, 4))) {
-        birth_year <- stringr::str_c(18, cnp_dec["AA"]) %>% as.integer()
+        birth_year <- stringr::str_c(18, cnp_dec["AA"])
         return(birth_year)
     }
 
     if (cnp_dec["S"] %in% as.character(c(5, 6))) {
-        birth_year <- stringr::str_c(20, cnp_dec["AA"])  %>% as.integer()
+        birth_year <- stringr::str_c(20, cnp_dec["AA"])
         return(birth_year)
     }
 
@@ -37,7 +44,8 @@ get_birth_year <- function(cnp_dec) {
     # the age of 100
     if (cnp_dec["S"] %in% c("7", "8") &&
         as.integer(cnp_dec["AA"]) <= this_year_2d) {
-        birth_year <- stringr::str_c(20, cnp_dec["AA"]) %>% as.integer()
+        birth_year <- glue::glue("{stringr::str_c(20, cnp_dec['AA'])} \\
+                                 or {stringr::str_c(19, cnp_dec['AA'])}")
         return(birth_year)
     } else {
         birth_year <- stringr::str_c(19, cnp_dec["AA"]) %>% as.integer()
