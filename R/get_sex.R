85339209ec6978482bf33363985512993bd7213c
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-10 22:14:58 +0000

    updates to get_sex

--- R/get_sex.R
+++ R/get_sex.R
@@ -1,20 +1,22 @@
-get_sex <- function(cnp_dec, lang = c("RO", "EN")) {
+get_sex <- function(cnp, lang = c("RO", "EN")) {
+
+    cnp_dec <- decompose_cnp(cnp)
 
     lang <- match.arg(lang)
 
     if (cnp_dec["S"] %in% as.character(c(1, 3, 5, 7))) {
-        gender_en <- "masculine"
-        gender_ro <- "masculin"
+        sex_en <- "masculine"
+        sex_ro <- "masculin"
     }
 
     if (cnp_dec["S"] %in% as.character(c(2, 4, 6, 8))) {
-        gender_en <- "feminine"
-        gender_ro <- "feminin"
+        sex_en <- "feminine"
+        sex_ro <- "feminin"
     }
 
     if (lang == "RO") {
-        return(gender_ro)
+        return(sex_ro)
     } else {
-        return(gender_en)
+        return(sex_en)
     }
 }
