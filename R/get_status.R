e787a41c7e7e3bb301769ac345e34a1734ffa71b
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-10 19:00:53 +0000

    update the implementation of get_status to match that of get_sex

--- R/get_status.R
+++ R/get_status.R
@@ -1,12 +1,20 @@
-get_status <- function(cnp_dec) {
+get_status <- function(cnp_dec, lang = c("RO", "EN")) {
+
+    lang <- match.arg(lang)
 
     if (cnp_dec["S"] %in% as.character(1:6)) {
-        status <- "native"
+        status_en <- "born in Romania"
+        status_ro <- "nascut in Romania"
     }
 
     if (cnp_dec["S"] %in% as.character(7:8)) {
-        status <- "resident"
+        status_en <- "resident"
+        status_ro <- "rezident"
     }
 
-    status
+    if (lang == "RO") {
+        return(status_ro)
+    } else {
+        return(status_en)
+    }
 }
