45c20fb9476f7bae7869d9fbe9398b29ee26b3ea
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-10 22:14:22 +0000

    add tests for get_status() and get_birth_year()

--- tests/testthat/test-get_birth_year.R
+++ tests/testthat/test-get_birth_year.R
@@ -7,5 +7,14 @@ test_that("get_birth_year works", {
 
     expect_equal(get_birth_year(7321206018072), "1932")
 
-    expect_equal(get_birth_year(49804232603242), "1898")
+    expect_equal(get_birth_year(4980423260322), "1898")
+})
+
+test_that("get_birth_year complains", {
+    expect_error(
+        get_birth_year(49804232603242),
+        "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()"
+    )
+
+    expect_error(get_birth_year("1940616346114"))
 })
--- tests/testthat/test-get_status.R
+++ tests/testthat/test-get_status.R
@@ -13,3 +13,13 @@ test_that("get_status works", {
     expect_equal(get_status(7041218318525, lang = "EN"), "resident")
 
 })
+
+test_that("get_status complains", {
+
+    expect_error(
+        get_status("6201206018078"),
+        "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()")
+
+    expect_error(get_status(62012060180782))
+
+})
