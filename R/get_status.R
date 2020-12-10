0410978346c55dae53d501952b0f97ece0c9acf6
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-10 22:15:13 +0000

    roxygen header for get_status

--- R/get_status.R
+++ R/get_status.R
@@ -1,4 +1,26 @@
-get_status <- function(cnp_dec, lang = c("RO", "EN")) {
+#' Extract Residence Status from the Personal Numeric Code
+#'
+#' @inheritParams interpret_cnp
+#'
+#' @return a string denoting residence status in the language of choice
+#' @export
+#'
+#' @examples
+#' get_status(6201206018078)
+#' get_status(5201206346491)
+#' get_status(1940616346114)
+#' get_status(1940616346114, lang = "EN")
+#' get_status(7041218318525)
+#' get_status(7041218318525, lang = "EN")
+get_status <- function(cnp, lang = c("RO", "EN")) {
+
+    if (!suppressMessages(check_cnp_is_valid(cnp))) {
+        stop(
+            "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()",
+            call. = FALSE)
+    }
+
+    cnp_dec <- decompose_cnp(cnp)
 
     lang <- match.arg(lang)
 
