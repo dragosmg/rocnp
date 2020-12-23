850142553aa7e49813d0f737ac1b0cd0b4228c98
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-23 15:09:49 +0000

    make `get_county` consistent in syntax and behaviour to other `get_...()` functions. test and document

--- R/get_county.R
+++ R/get_county.R
@@ -1,16 +1,28 @@
-get_county <- function(cnp_dec) {
+#' Extract County of Issue from the Personal Numeric Code
+#'
+#' @inheritParams interpret_cnp
+#'
+#' @return a string representing the name of the county where the CNP was issued
+#' @export
+#'
+#' @examples
+#' get_county(6201206018078)
+#' get_county(5201206346491)
+#' get_county(1940616346114)
+#' get_county(7041218318525)
+get_county <- function(cnp) {
+
+    if (!suppressMessages(check_cnp_is_valid(cnp))) {
+        stop(
+            "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()",
+            call. = FALSE)
+    }
+
+    cnp_dec <- decompose_cnp(cnp)
+
     county_code <- cnp_dec["JJ"]
 
-    county_lookup_file <- system.file("extdata", "county_lookup.csv",
-                                      package = "rocnp")
-
-    county_lut <- readr::read_csv(county_lookup_file,
-                                  col_types = readr::cols(
-                                      code = readr::col_character(),
-                                      county = readr::col_character()
-                                  ))
-
-    county_lut %>%
+    county_lookup %>%
         dplyr::filter(.data$code == county_code) %>%
         dplyr::pull(.data$county)
 }
