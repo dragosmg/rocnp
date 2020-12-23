c0a0904a8b17998b6d6f7b3518b84183c4fb4680
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-23 19:22:16 +0000

    update get_sex() tests (removal of lang arg)

--- tests/testthat/test-get_sex.R
+++ tests/testthat/test-get_sex.R
@@ -1,22 +1,19 @@
 test_that("get_sex works", {
-    expect_equal(get_sex(1940616346114), "masculin")
-    expect_equal(get_sex(1940616346114, lang = "EN"), "masculine")
+    expect_equal(get_sex(1940616346114), "M")
 
-    expect_equal(get_sex(7041218318525), "masculin")
-    expect_equal(get_sex(7041218318525, lang = "EN"), "masculine")
+    expect_equal(get_sex(7041218318525), "M")
 
-    expect_equal(get_sex(6201206018078), "feminin")
-    expect_equal(get_sex(6201206018078, lang = "EN"), "feminine")
+    expect_equal(get_sex(6201206018078), "F")
 
-    expect_equal(get_sex(7321206018072), "masculin")
+    expect_equal(get_sex(7321206018072), "M")
 
-    expect_equal(get_sex(4980423260322), "feminin")
+    expect_equal(get_sex(4980423260322), "F")
 })
 
 test_that("get_sex complains", {
     expect_error(
         get_sex(49804232603242),
-        "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()"
+        "Please supply a vector of valid CNPs. At least one of the supplied values is not valid. For diagnosis use check_cnp_is_valid()"
     )
 
     expect_error(get_sex("1940616346114"))
