0c8e188e15bb11757b5604f9b818d8baa1916b41
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-29 20:43:23 +0000

    simplify the implementation of get_birth_month() to return an integer instead of a choice between a string and an integer
    * removed the language and type arguments since no longer necessary

--- R/get_birth_month.R
+++ R/get_birth_month.R
@@ -1,34 +1,19 @@
 #' Extract the Month of Birth from the Personal Numeric Code
 #'
 #' This function extracts the month of birth starting from the `"LL"` component.
-#' The month returned either as string (`long` and `short` variants) or numeric,
-#' in the specified language, the default being Romanian.
 #'
 #' @inheritParams get_sex
-#' @param lang a string denoting the language for the interpretation - either
-#'     `RO` for Romanian (the default) or `EN` for English.
 #'
-#' @param type string indicating type of output desired, one of `"long"`, full
-#'     month name (default), `"short"`, short month name, and `"numeric"`
-#'
-#' @return a string or numeric depending on the requested output
+#' @return a numeric vector representing the month of birth
 #' @export
 #'
 #' @examples
 #' get_birth_month(1940616346114)
-#' get_birth_month(1940616346114, type = "short")
-#' get_birth_month(1940616346114, type = "numeric")
-#' get_birth_month(7041218318525, lang = "EN")
-#' get_birth_month(7041218318525, lang = "EN", type = "long")
-#' get_birth_month(7041218318525, lang = "EN", type = "numeric")
-#' get_birth_month(7041218318525, lang = "EN", type = "short")
+#' get_birth_month(7041218318525)
 #' cnps <- c(5201206346491, 1940616346114, 7041218318525, 6201206018078)
-#' get_birth_month(cnps, lang = "EN", type = "short")
-#' get_birth_month(cnps, lang = "RO")
+#' get_birth_month(cnps)
 #' get_birth_month(c(5201206346491, 1940616346114, 7041218318525, NA))
-get_birth_month <- function(cnp,
-                            lang = c("RO", "EN"),
-                            type = c("long", "numeric", "short")) {
+get_birth_month <- function(cnp) {
 
     suppressMessages(
         checks <- check_cnp_is_valid(cnp)
@@ -45,55 +30,19 @@ get_birth_month <- function(cnp,
 
     cnp_dec <- purrr::map(cnp, decompose_cnp)
 
-    lang <- match.arg(lang)
 
-    type <- match.arg(type)
-
-    arg_list <- list(cnp_dec = cnp_dec,
-                     lang = rep(lang, length(cnp_dec)),
-                     type = rep(type, length(cnp_dec)))
-
-    if (type == "numeric") {
-        result <- purrr::pmap_dbl(arg_list, get_birth_month_unvec)
-    } else {
-        result <- purrr::pmap_chr(arg_list, get_birth_month_unvec)
-    }
+    result <- purrr::map_int(cnp_dec, get_birth_month_unvec)
 
     result
 }
 
 get_birth_month_unvec <- function(cnp_dec, lang, type) {
 
-    # define a vector of month names in Romanian
-    month_name_ro <- c("Ianuarie", "Februarie", "Martie", "Aprilie", "Mai",
-                       "Iunie", "Iulie", "August", "Septembrie", "Octombrie",
-                       "Noiembrie", "Decembrie")
-
-    if (is.na(cnp_dec["LL"]) && type != "numeric") {
-        return(NA_character_)
-    }
-
-    if (is.na(cnp_dec["LL"]) && type == "numeric") {
+    if (is.na(cnp_dec["LL"])) {
         return(NA_integer_)
     }
 
-    if (type == "numeric") {
-        month <- as.numeric(cnp_dec["LL"])
-        return(month)
-    }
+    month <- as.integer(cnp_dec["LL"])
 
-    if (lang == "RO" && type == "long") {
-        month <- month_name_ro[as.numeric(cnp_dec["LL"])]
-        return(month)
-    } else if (lang == "RO" && type == "short") {
-        month <- month_name_ro[as.numeric(cnp_dec["LL"])] %>%
-            stringr::str_sub(start = 1L, end = 3L)
-        return(month)
-    } else if (lang == "EN" && type == "long") {
-        month <- month.name[as.numeric(cnp_dec["LL"])]
-        return(month)
-    } else {
-        month <- month.abb[as.numeric(cnp_dec["LL"])]
-        return(month)
-    }
+    month
 }
