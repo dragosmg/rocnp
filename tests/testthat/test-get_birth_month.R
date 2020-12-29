dd36ac332cce046b36e72e29d34f823a0ad1e725
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2020-12-29 16:09:24 +0000

    added tests for get_birth_month() to cover vectorisation and NA contagion

--- tests/testthat/test-get_birth_month.R
+++ tests/testthat/test-get_birth_month.R
@@ -1,4 +1,24 @@
 test_that("get_birth_month works", {
+
+    valid_cnps <- c(1940616346114, 7041218318525, 4980423260322, NA)
+    month_vec_ro <- c("Iunie", "Decembrie", "Aprilie", NA_character_)
+    month_vec_ro_short <- c("Iun", "Dec", "Apr", NA_character_)
+    month_vec_eng <- c("June", "December", "April", NA_character_)
+    month_vec_eng_short <- c("Jun", "Dec", "Apr", NA_character_)
+    month_vec_num <- c(6, 12, 4, NA_integer_)
+
+    expect_equal(get_birth_month(valid_cnps), month_vec_ro)
+    expect_equal(get_birth_month(valid_cnps, type = "short"),
+                 month_vec_ro_short)
+
+    expect_equal(get_birth_month(valid_cnps, lang = "EN", type = "long"),
+                 month_vec_eng)
+    expect_equal(get_birth_month(valid_cnps, lang = "EN", type = "short"),
+                 month_vec_eng_short)
+
+    expect_equal(get_birth_month(valid_cnps, type = "numeric"),
+                 month_vec_num)
+
     expect_equal(get_birth_month(1940616346114), "Iunie")
     expect_equal(get_birth_month(1940616346114, type = "numeric"), 6)
     expect_equal(get_birth_month(1940616346114, type = "short"), "Iun")
@@ -24,7 +44,7 @@ test_that("get_birth_month works", {
 test_that("get_birth_month complains", {
     expect_error(
         get_birth_month(49804232603242),
-        "Please supply a valid CNP. For diagnosis use check_cnp_is_valid()"
+        "Please supply a vector of valid CNPs. The input vector has 1 invalid values. For a detailed diagnosis use check_cnp_is_valid()"
     )
 
     expect_error(get_birth_month("1940616346114"))
