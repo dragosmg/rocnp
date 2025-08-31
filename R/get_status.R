11a55942801c709914e3163207a4a93bc3403da7
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-08-31 18:21:45 +0100

    docs

--- R/get_status.R
+++ R/get_status.R
@@ -7,12 +7,8 @@
 #' @export
 #'
 #' @examples
-#' get_status(6201206018078)
-#' get_status(5201206346491)
-#' get_status(1940616346114)
-#' get_status(1940616346114, lang = "EN")
-#' get_status(7041218318525)
-#' get_status(7041218318525, lang = "EN")
+#' get_status(cnp("6201206018078"))
+#' get_status(cnp(c("5201206346491", "1940616346114")))
 get_status <- function(cnp) {
   vctrs::field(cnp, "status")
 }
