5231acf4119869ca2784fc1b75a46dce57c19da7
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-08-31 13:55:58 +0100

    new constructors for the cnp class

--- R/cnp.R
+++ R/cnp.R
@@ -1,30 +1,47 @@
+# developer-facing constructor for the `cnp` S3 class
 new_cnp <- function(x = character())  {
 
   if (!is.character(x)) {
     cli::cli_abort(
-      "{.arg x} must be a character. You have supplied a {.obj_type_friendly x}.",
+      "{.arg x} must be a character. You have supplied a \\
+      {.obj_type_friendly x}.",
       call = rlang::caller_env()
     )
   }
-
+# browser()
   decomposed_cnp <- decompose_cnp(x)
 
+  augmented_cnp <- augment_cnp(decomposed_cnp)
+
   vctrs::new_rcrd(
-    decomposed_cnp,
+    augmented_cnp,
     class = "cnp"
   )
+
 }
 
+#' Create a `cnp` object
+#'
+#' @param x (character) a character (or values that can be cast to character)
+#'   vector of personal numeric codes (CNPs).
+#'
+#' @return a vector of class `cnp`.
+#' @export
+#'
+#' @examples
+#' cnp(c("1940616346114", "7041218318525"))
 cnp <- function(x = character()) {
+
   x <- as.character(x)
 
-  if (any(nchar(x) != 13)) {
+  if (any(nchar(x) != 13, na.rm = TRUE)) {
     cli::cli_abort(
       "Each element of {.arg x} must have exactly 13 characters."
     )
   }
 
-  new_cnp(x)
+  validate_cnp(new_cnp(x))
+
 }
 
 #' @export
