995c36350275ffabb86676adfaebe09e9f1b75b3
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-08-31 13:57:59 +0100

    initial commit of `augment_cnp()`

---
+++ R/augment_cnp.R
@@ -0,0 +1,67 @@
+#' Parse the CNP fields
+#'
+#' @param x (list) a decomposed CNP.
+#'
+#' @return th input list with a series of additional fields:
+#'   * `sex`: the parsed sex
+#'   * `dob`: the reconstructed date of birth
+#'
+#' @keywords internal
+#'
+#' @examples
+augment_cnp <- function(x) {
+  # browser()
+
+  output <- x |>
+    parse_sex() |>
+    parse_dob()
+
+  output
+}
+
+parse_sex <- function(x) {
+  sex_field <- x[["s"]]
+
+  sex <- dplyr::case_when(
+    sex_field %in% c("1", "3", "5", "7") ~ "M",
+    sex_field %in% c("2", "4", "6", "8") ~ "F"
+  )
+
+  x$sex <- sex
+
+  x
+}
+
+parse_dob <- function(x) {
+
+  current_year <- lubridate::today() |>
+    lubridate::year() - 2000
+
+  dob_df <- tibble::tibble(
+    sex = x[["s"]],
+    year = x[["aa"]],
+    month = x[["ll"]],
+    day = x[["zz"]]
+  ) |>
+    dplyr::mutate(
+      century_digits = dplyr::case_when(
+        sex %in% c("1", "2") ~ "19",
+        sex %in% c("3", "4") ~ "18",
+        sex %in% c("5", "6") ~ "20",
+        sex %in% c("7", "8") & as.integer(year) <= current_year ~ "20",
+        sex %in% c("7", "8") & as.integer(year) >= current_year ~ "19",
+        .default = NA_character_
+      )
+    ) |>
+    dplyr::mutate(
+      birth_year = stringr::str_c(century_digits, year),
+      dob = glue::glue("{birth_year}-{month}-{day}"),
+      dob = lubridate::ymd(dob)
+    )
+
+  dob <- dplyr::pull(dob_df, dob)
+
+  x$dob <- dob
+
+  x
+}
