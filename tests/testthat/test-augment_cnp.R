fe11e92e72a2c17b471401378557fa176f36849f
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-09-14 14:30:15 +0100

    tests for `parse_yob()` and `parse_status()`

--- tests/testthat/test-augment_cnp.R
+++ tests/testthat/test-augment_cnp.R
@@ -13,3 +13,75 @@ test_that("parse_sex() works", {
     c("M", "F", "M", "F", "M", "F", "M", "F")
   )
 })
+
+test_that("parse_yob() works", {
+  valid_cnp <- c("1940616346114", "7041218318525", "4980423260322", NA)
+
+  expect_snapshot(
+    parse_yob(decompose_cnp(valid_cnp))
+  )
+
+  expect_identical(
+    parse_yob(decompose_cnp(valid_cnp))[["yob"]],
+    c("1994", "2004", "1898", NA)
+  )
+})
+
+test_that("parse_dob() works", {
+  valid_cnp <- c("1940616346114", "7041218318525", "4980423260322", NA)
+
+  expect_snapshot(
+    valid_cnp |>
+      decompose_cnp() |>
+      parse_yob() |>
+      parse_dob()
+  )
+
+  cnp <-  valid_cnp |>
+    decompose_cnp() |>
+    parse_yob() |>
+    parse_dob()
+
+  expect_identical(
+    cnp[["dob"]],
+    as.Date(c("1994-06-16", "2004-12-18", "1898-04-23", NA))
+  )
+})
+
+test_that("parse_county() works", {
+  valid_cnp <- c("1940616346114", "7041218318525", "4980423260322", NA)
+
+  expect_snapshot(
+    valid_cnp |>
+      decompose_cnp() |>
+      parse_county()
+  )
+
+  cnp <- valid_cnp |>
+    decompose_cnp() |>
+    parse_county()
+
+  expect_identical(
+    cnp[["county"]],
+    c("Teleorman", "Sălaj", "Mureș", NA)
+  )
+})
+
+test_that("parse_status() works", {
+  valid_cnp <- c("1940616346114", "7041218318525", "4980423260322", NA)
+
+  expect_snapshot(
+    valid_cnp |>
+      decompose_cnp() |>
+      parse_status()
+  )
+
+  cnp <- valid_cnp |>
+    decompose_cnp() |>
+    parse_status()
+
+  expect_identical(
+    cnp[["status"]],
+    c("native", "resident", "native", NA)
+  )
+})
